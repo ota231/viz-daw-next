f8364a1b898f7cb4cab167038f3e8a38f41deba0
Tomisin <ota231@nyu.edu>
2024-12-04 20:11:09 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React from 'react';
+
+import { Heading, Text, Flex, Button, GlitchFx, LetterFx, Arrow } from '@/once-ui/components';
+import Link from 'next/link';
+
+export default function Home() {
+
+	return (
+		<Flex
+			fillWidth paddingTop="l" paddingX="l"
+			direction="column" alignItems="center" flex={1}>
+			<Flex
+				position="relative"
+				as="section" overflow="hidden"
+				fillWidth minHeight="0" maxWidth={68}
+				direction="column" alignItems="center" flex={1}>
+				<Flex
+					as="main"
+					direction="column" justifyContent="center"
+					fillWidth fillHeight padding="l" gap="l">
+					<Flex
+						mobileDirection="column"
+						fillWidth gap="24">
+						<Flex
+							position="relative"
+							flex={2} paddingTop="12">
+							<Heading variant="display-strong-s"> <GlitchFx interval={5000}>TunedIn</GlitchFx></Heading>
+						</Flex>
+						<Flex
+							position="relative"
+							flex={6} gap="24" marginBottom="104"
+							direction="column">
+							<Heading
+								wrap="balance"
+								variant="display-strong-s">
+								<span className="font-code">
+									<LetterFx
+										trigger="instant">
+										To help you learn about the DAW experience for visually impaired users.
+									</LetterFx>
+								</span>
+							</Heading>
+							<Button
+								id="readDocs"
+								href="./learning"
+								variant="secondary">
+								<Flex alignItems="center">
+									Let's Learn!
+									<Arrow trigger="#readDocs" />
+								</Flex>
+							</Button>
+						</Flex>
+					</Flex>
+					<Flex position="relative">
+						<Text >
+							Final Project for Musical Abilities & Disabilities by: Tomisin Adeyemi
+						</Text>
+					</Flex>
+				</Flex>
+			</Flex>
+			<Flex
+				as="footer"
+				position="relative"
+				fillWidth paddingX="l" paddingY="m"
+				justifyContent="space-between">
+				<Text
+					variant="body-default-s" onBackground="neutral-weak">
+					© 2024 Once UI, <Link href="https://github.com/once-ui-system/nextjs-starter?tab=MIT-1-ov-file">MIT License</Link>
+				</Text>
+				<Flex
+					gap="12">
+					<Button
+						href="https://github.com/once-ui-system/nextjs-starter"
+						prefixIcon="github" size="s" variant="tertiary">
+						GitHub
+					</Button>
+				</Flex>
+			</Flex>
+		</Flex>
+	);
+}
---
+++ src/once-ui/resources/config.js
@@ -0,0 +1,47 @@
+const baseURL = 'demo.once-ui.com'
+
+// default customization applied to the HTML in the main layout.tsx
+const style = {
+    theme:       'dark',        // dark | light
+    neutral:     'gray',        // sand | gray | slate
+    brand:       'blue',        // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent:      'orange',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    solid:       'contrast',    // color | contrast
+    solidStyle:  'flat',        // flat | plastic
+    border:      'rounded',     // rounded | playful | conservative
+    surface:     'filled', // filled | translucent
+    transition:  'all',         // all | micro | macro
+    scaling:     '100',         // 90 | 95 | 100 | 105 | 110
+}
+
+// default metadata
+const meta = {
+    title: 'TunedIn',
+    description: 'final proj for musical abilities and disabilites'
+}
+
+
+// default open graph data
+const og = {
+    title: 'Once UI for Next.js',
+    description: 'We let designers code and developers design.',
+    type: 'website'
+}
+
+// default schema data
+const schema = {
+    logo: '',
+    type: 'Organization',
+    name: 'One UI',
+    description: 'Once UI is an open-source design system and component library for Next.js.',
+    email: 'lorant@once-ui.com'
+}
+
+// social links
+const social = {
+    twitter: 'https://www.twitter.com/_onceui',
+    linkedin: 'https://www.linkedin.com/company/once-ui/',
+    discord: 'https://discord.com/invite/5EyAQ4eNdS'
+}
+
+export { baseURL, style, meta, og, schema, social };
